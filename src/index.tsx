4d072484f83f76a7856085a273c9f317408a0eca
apurvathakker9 <atapurvathakker906@gmail.com>
2023-06-03 17:55:34 +0530

    updated css in code and links removed

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/css/plugins.css';
+import './assets/css/style.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(
