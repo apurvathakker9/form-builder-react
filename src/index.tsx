b58a327d600d52004dfac54fafd6b3272aa4dd21
apurvathakker9 <atapurvathakker906@gmail.com>
2023-06-03 17:28:38 +0530

    Styles now in folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './plugins.css';
+import './style.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(
